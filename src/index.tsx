5f6628a1affdeb4a17167807aac0f4c02c11d766
Dima <dpolevodin@gmail.com>
2022-03-02 13:49:50 +0500

    added HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import './index.css';
+
+//TODO:При деплое на сервер поменять HashRouter на BrowserRouter
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
